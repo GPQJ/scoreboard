7f6cd92f981fc7bdb4965f5d3b41269a6f8fd909
Gisell <136038328+GPQJ@users.noreply.github.com>
2024-10-05 17:38:16 -0500

    highlighted winning in .js
    updated OnTopTeam function

--- scoreboard.js
+++ scoreboard.js
@@ -7,15 +7,19 @@ let guestActiveScore = 0
 function OnTopTeam() {
 	
 	homeScore.style.border = guestScore.style.border = "var(--blackborder)"
+	homeScore.style.boxShadow = guestScore.style.boxShadow = "none"
+	homeScore.style.textShadow = guestScore.style.textShadow = "none"
 	
 	if (homeActiveScore > guestActiveScore) {
-		homeScore.style.border = "var(--redborder)";
+		homeScore.style.border = "var(--redborder)"
+		homeScore.style.boxShadow = homeScore.style.textShadow = "var(--redshadow)"
 	}
 	else if (guestActiveScore > homeActiveScore) {
-		guestScore.style.border = "var(--redborder)";
+		guestScore.style.border = "var(--redborder)"
+		guestScore.style.boxShadow = guestScore.style.textShadow = "var(--redshadow)"
 	}
 	else {
-		homeScore.style.border = guestScore.style.border = "var(--redborder)";
+		homeScore.style.border = guestScore.style.border = "var(--redborder)"
 	}
 }
 
